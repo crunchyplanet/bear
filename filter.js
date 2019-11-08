0735f51cd5f0ab8fc9588284ef87ee36bcbdf709
crunchyplanet <luojinghan.x@gmail.com>
2019-11-07 22:32:08 -0800

    home update

--- filter.js
+++ filter.js
@@ -11,12 +11,12 @@ for (let i = 0; i < ftrs.length; i++) {
     ftrs[i].addEventListener("click", function () {
         let ctg = ftrs[i].textContent;
         console.log(ctg);
-        if (ctg == "All") {
-            for (let i = 0; i < boxes.length; i++) {
-                boxes[i].style.display = "inline-block";
-            }
-        }
-        if (ctg == "UX Design") {
+        // if (ctg == "All") {
+        //     for (let i = 0; i < boxes.length; i++) {
+        //         boxes[i].style.display = "inline-block";
+        //     }
+        // }
+        if (ctg == "Product Design") {
             for (let i = 0; i < boxes.length; i++) {
                 boxes[i].style.display = "inline-block";
                 if (boxes[i].className != "box ux") {
@@ -24,7 +24,7 @@ for (let i = 0; i < ftrs.length; i++) {
                 }
             }
         }
-        if (ctg == "Graphic/Motion Design") {
+        if (ctg == "Motion Graphics") {
             for (let i = 0; i < boxes.length; i++) {
                 boxes[i].style.display = "inline-block";
                 if (boxes[i].className != "box gm") {
@@ -32,14 +32,14 @@ for (let i = 0; i < ftrs.length; i++) {
                 }
             }
         }
-        if (ctg == "Animation") {
-            for (let i = 0; i < boxes.length; i++) {
-                boxes[i].style.display = "inline-block";
-                if (boxes[i].className != "box animation") {
-                    boxes[i].style.display = "none";
-                }
-            }
-        }
+        // if (ctg == "Animation") {
+        //     for (let i = 0; i < boxes.length; i++) {
+        //         boxes[i].style.display = "inline-block";
+        //         if (boxes[i].className != "box animation") {
+        //             boxes[i].style.display = "none";
+        //         }
+        //     }
+        // }
     });
 }
 
